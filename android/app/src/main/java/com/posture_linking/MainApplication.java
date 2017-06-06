f1febfb8fa6ab479c29cfaef3a29fb01ee50bc10
JohnnyYu0930 <a3660980@gmail.com>
2017-06-07 01:21:57 +0800

    add ios config

--- android/app/src/main/java/com/posture_linking/MainApplication.java
+++ android/app/src/main/java/com/posture_linking/MainApplication.java
@@ -3,6 +3,8 @@ package com.posture_linking;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
