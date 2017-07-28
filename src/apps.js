af2ae90a7e3fed96b4def2ab01677f8daa3fe753
JohnnyYu0930 <a3660980@gmail.com>
2017-07-29 00:23:27 +0800

    add ios version UI

--- src/apps.js
+++ src/apps.js
@@ -3,6 +3,7 @@ import {
 } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
+import { iconsMap, iconsLoaded } from '../res/icons';
 
 registerScreens();
 
@@ -18,7 +19,8 @@ export const startMainApp = () => {
     drawer: {
     	left: {
     		screen: 'SideMenuScreen'
-    	}
+      },
+      disableOpenGesture: false
     }
   });
 };
@@ -36,4 +38,69 @@ export const startLoginApp = () => {
       orientation: 'portrait'
     }
   });
+};
+
+
+export const startiOSMainApp = () => {
+  Navigation.startTabBasedApp({
+  tabs: [
+    {
+      label: R.strings.COMBINATION_TITLE,
+      screen: 'CombinationScreen',
+      title: R.strings.COMBINATION_TITLE,
+      icon: iconsMap['extension'],
+      iconInsets: { 
+        top: 3, 
+        left: 0,
+        bottom: -3, 
+        right: 0
+      },
+    },
+    {
+      label: R.strings.BUTTON_LIST_TITLE,
+      screen: 'ButtonListScreen',
+      title: R.strings.BUTTON_LIST_TITLE,
+      icon: iconsMap['touch-app'],
+      iconInsets: { 
+        top: 1, 
+        left: 0,
+        bottom: -1, 
+        right: 0
+      },
+    },
+    {
+      label: R.strings.SERVICE_LIST_TITLE,
+      screen: 'ServiceListScreen', 
+      icon: iconsMap['room-service'],
+      iconInsets: { 
+        top: 3, 
+        left: 0,
+        bottom: -3, 
+        right: 0
+      },
+      title: R.strings.SERVICE_LIST_TITLE,
+    },
+    {
+      label: '選單',
+      screen: 'SideMenuScreen', 
+      icon: iconsMap['menu'],
+      iconInsets: { 
+        top: 3, 
+        left: 0,
+        bottom: -3, 
+        right: 0
+      },
+      title: R.strings.SERVICE_LIST_TITLE,
+    }
+  ],
+  tabsStyle: {
+    tabBarButtonColor: '#bbb', 
+    tabBarSelectedButtonColor: R.colors.NAVBAR_BACKGROUND,
+    tabBarBackgroundColor: R.colors.WHITE_TEXT
+  },
+  appStyle: {
+    orientation: 'portrait' 
+  },
+  passProps: {},
+});
 };
